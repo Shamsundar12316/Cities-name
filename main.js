760c4cb1a50ba2df5dfdebbd74a9912c431f8395
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 03:54:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// part 32 // question 38
+// cities
+var karachi = "karachi is in pakistan";
+var paris = "paris is in france";
+var barcelona = "barcelona is in spain";
+function call_city(city) {
+    console.log(city);
+}
+console.log(paris, barcelona, karachi);
---
+++ main.ts
@@ -0,0 +1,10 @@
+// part 32 // question 38
+// cities
+let karachi = "karachi is in pakistan"
+let paris = "paris is in france"
+let barcelona = "barcelona is in spain"
+
+function call_city(city: string,){
+    console.log(city)
+}
+console.log(paris,barcelona,karachi)
